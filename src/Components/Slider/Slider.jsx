d3a39dee723031d72dcfee9263b882abbde8cba2
Vishnu Dabi <xyz14203@gmail.com>
2024-03-13 17:43:05 +0530

    updates

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -32,18 +32,6 @@ function SwipeToSlide() {
         <div>
           <h3>5</h3>
         </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
-        <div>
-          <h3>9</h3>
-        </div>
       </Slider>
     </div>
   );
