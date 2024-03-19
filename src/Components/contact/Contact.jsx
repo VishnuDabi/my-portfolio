a36405a35d672985f67134eeaafb591174456977
Vishnu Dabi <xyz14203@gmail.com>
2024-03-19 18:35:27 +0530

    contact form updates

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -28,8 +28,60 @@ const Contact = () => {
           </div>
         )}
         {formShow && (
-          <div>
-            <form onSubmit={(event) => submitHandler(event)}></form>
+          <div className={`text__container active`}>
+            <div className="flex__container">
+              <div className="flex__child">
+                <h2 className="text__black">Get in touch</h2>
+                <div
+                  className="close-container"
+                  id="close_btn"
+                  onClick={handleShow}
+                >
+                  <div className="leftright"></div>
+                  <div className="rightleft"></div>
+                  <label className="close label-close" htmlFor="close_btn">
+                    close
+                  </label>
+                </div>
+                <form
+                  className="contact__form"
+                  onSubmit={(event) => submitHandler(event)}
+                >
+                  <div>
+                    <label htmlFor="name">Name</label>
+                    <input
+                      type="text"
+                      id="name"
+                      placeholder="Enter Your Name"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="email"
+                      id="email"
+                      placeholder="Enter Your Email ID"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="subject">Subject</label>
+                    <input type="text" placeholder="Subject" id="subject" />
+                  </div>
+                  <div>
+                    <label htmlFor="massage">Massage</label>
+                    <textarea
+                      type="text"
+                      id="massage"
+                      placeholder="Enter Massage"
+                    />
+                  </div>
+                  <button className="btn__dark">Submit</button>
+                </form>
+              </div>
+              <div className="flex__child">
+                <img className="img" src="./about.jpeg" alt="" />
+              </div>
+            </div>
           </div>
         )}
       </div>
